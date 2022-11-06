4c84b718650eefa65705fc4a002bbb2817629fe4
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2022-11-06 23:18:55 +0200

    first

---
+++ server/routes/feedback.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const {feedback} = require('../models');
+
+const router = express.Router();
+
+
+router.get('/', async (req,res) => {
+    const feedbackList = await feedback.findAll();
+    res.status(200).json(feedbackList);
+});
+
+router.post('/', async (req,res) => {
+    const Feedback = req.body;
+    await feedback.create(Feedback);
+    res.status(200).json(Feedback);
+});
+
+router.delete(`/byId/:id`, async (req,res) => {
+    const id = req.params.id;
+    if (!id)
+    {
+        res.status(404).json({message: 'There is not that comment'})
+    }
+    const Feedback = await feedback.destroy({
+        where: {ID_Feedback: id}})
+        if (!Feedback) {
+            res.status(464).json({message: 'Failed to delete comment'})
+        }
+        else{
+            res.status(200).json(Feedback);
+        }
+});
+
+module.exports = router;
